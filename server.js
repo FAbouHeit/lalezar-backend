7f9a79a71d92e1aed76fb9eed7d68ea9622a4849
Fuad Abou Heit <f.abouheit@gmail.com>
2024-01-09 14:27:34 +0200

    set up backend server and mongo

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-const hello = "hello_team"
+import express from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connect from './config/Config.js';
+dotenv.config();
+
+const PORT = process.env.PORT || 6666;
+const app = express();
+app.use(express.json());
+
+
+const corsOption = {
+    origin: process.env.FRONT_END_PATH,
+    credentials: true,
+    optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOption));
+
+app.listen(PORT, ()=>{
+    connect();
+    console.log(`running on port: ${PORT}`);
+    if(PORT === 6666){
+        console.log("ERROR: issue reading port from process.env. Continue with caution! ...");
+    }
+})
